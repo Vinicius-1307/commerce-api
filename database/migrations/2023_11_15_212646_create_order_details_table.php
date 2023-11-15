a4dbf82b4464a2dcd8b4f10c754e47213f9ffca0
Vinicius-1307 <viniciusjose9@outlook.com>
2023-11-15 18:40:45 -0300

    fix: change name of table

--- database/migrations/2023_11_15_212646_create_order_details_table.php
+++ database/migrations/2023_11_15_212646_create_order_details_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateOrdersDetailsTable extends Migration
+class CreateOrderDetailsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateOrdersDetailsTable extends Migration
      */
     public function up()
     {
-        Schema::create('orders_details', function (Blueprint $table) {
+        Schema::create('order_details', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('order_id')->constrained('orders')->onDelete('CASCADE');
             $table->unsignedBigInteger('product_id')->constrained('products')->onDelete('CASCADE');
@@ -29,6 +29,6 @@ class CreateOrdersDetailsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('orders_details');
+        Schema::dropIfExists('order_details');
     }
 }
